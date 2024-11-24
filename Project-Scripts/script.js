e1bf169ab19d67470ffd886371d005334b348c5c
eliomarvinicius <138163810+eliomarvinicius@users.noreply.github.com>
2024-11-24 14:56:37 -0300

    clone

---
+++ Project-Scripts/script.js
@@ -0,0 +1,29 @@
+function navegarUrlAtividade(){
+    window.location.href = 'Atividades.html'
+}
+
+function navegarUrlQuemSomos(){
+    window.location.href = 'QuemSomos.html'
+}
+
+function navegarUrlContribua(){
+    window.location.href = 'Contribua.html'
+}
+
+function ChatWhatsApp(){
+    window.location.href = 'https://wa.me/558189543669'
+}
+
+function ChatInstagram(){
+    window.location.href = 'https://www.instagram.com/institutoalerta/'
+}
+
+// Função do header celular
+const navMenu = document.querySelector('.navigation-links');
+
+const menu = document.querySelector('.menu');
+
+menu.addEventListener('click', ()=>{
+    menu.classList.toggle('ativo');
+    navMenu.classList.toggle('ativo');
+})
